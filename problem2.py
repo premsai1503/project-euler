a028bd0b2fd4f5aa1079bf7d8aadb90cefc6b1d4
yeshwanthd <56600297+yeshwanthd@users.noreply.github.com>
2022-10-31 19:28:32 +0530

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -1,6 +1,10 @@
 
 s, x, y = 0, 0, 2
 L = int(input('Sum of even Fibonacci numbers <'))
+i = 0
+while i > 0:
+    print("this is the change")
+    
 while y < L:
     s, x, y = s+y, y, 4*y+x
 print ("is", s)
